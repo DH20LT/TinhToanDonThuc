caf5f843b7aceb6a113daec42a4604ea3ebd1d5f
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2021-08-12 21:12:52 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonomialCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
